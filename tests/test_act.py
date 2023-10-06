793d8e1f3e1a1b3c5300384d450e31cfe991a2e6
Hidde Jense <JenseH@cardiff.ac.uk>
2023-10-06 12:16:13 +0100

    Initial test!

---
+++ tests/test_act.py
@@ -0,0 +1,31 @@
+"""
+This test checks that the ACT DR6 CMB-only likelihood is correctly installed
+and working properly.
+"""
+import pytest
+import numpy as np
+from cobaya.model import get_model
+
+info = {
+    "params": {
+        "ombh2": 0.022,
+        "omch2": 0.117,
+        "ns": 0.96,
+        "As": 2e-9,
+        "tau": 0.065,
+        "cosmomc_theta": 104.09e-4
+    },
+    "theory": {"camb": {"extra_args": {"lens_potential_accuracy": 1}}},
+    "likelihood": {},
+    "sampler": {"evaluate": None},
+    "debug": True
+}
+
+def test_import():
+    import act_dr6_cmbonly
+
+def test_model():
+    info["likelihood"] = {
+        "act_dr6_cmbonly.ACTDR6CMBonly": {}
+    }
+    model = get_model(info)  # noqa F841
