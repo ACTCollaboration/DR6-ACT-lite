9e3d42120b245135fe61cf66e2f792129fc4280b
Hidde Jense <JenseH@cardiff.ac.uk>
2023-10-09 13:59:29 +0100

    De-tox tests.

--- setup.py
+++ setup.py
@@ -1,27 +1,15 @@
 from setuptools import setup
-import os
-import re
-
-file_dir = os.path.abspath(os.path.dirname(__file__))
-
-
-def get_version():
-    fp = open(os.path.join(file_dir, "act_dr6_cmbonly", "__init__.py")).read()
-    version = re.search(r"^__version__ = \"([^\"]*)\"", fp, re.M)
-
-    if version:
-        return version.group(1)
-
-    raise RuntimeError("Failed to find version string.")
+from act_dr6_cmbonly import __author__, __version__
 
 
 setup(name="ACT DR6 CMBonly",
-      version=get_version(),
+      version=__version__,
       description="Cobaya likelihood for the DR6 foreground-marginalized data",
-      author="Hidde T. Jense",
+      author=__author__,
       url="https://github.com/ACTCollaboration/dr6-cmbonly",
       install_requires=["sacc>=0.12.0","cobaya>=3.3"],
       packages = ["act_dr6_cmbonly"],
-      package_data={
-          "testpaths":["act_dr6_cmbonly"]
-      })
+      test_suite="act_dr6_cmbonly.tests",
+      classifiers=[
+          "Programming Language :: Python :: 3"
+      ])
