0e1f38e534394378965f9670c37d93301c4ad892
Hidde Jense <JenseH@cardiff.ac.uk>
2023-10-10 16:49:26 +0100

    Update setup.py
    Add tests to install data.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name="ACT DR6 CMBonly",
       url="https://github.com/ACTCollaboration/dr6-cmbonly",
       install_requires=["sacc>=0.12.0", "cobaya>=3.3"],
       packages=["act_dr6_cmbonly"],
-      package_data={"act_dr6_cmbonly": ["*.yaml", "data/*"]},
+      package_data={"act_dr6_cmbonly": ["*.yaml", "data/*", "tests/*"]},
       test_suite="act_dr6_cmbonly.tests",
       classifiers=[
           "Programming Language :: Python :: 3"
