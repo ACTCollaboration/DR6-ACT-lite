a9ef81f91249adcbd42273ec987df5587d39ba7f
Hidde Jense <JenseH@cardiff.ac.uk>
2023-11-06 14:38:12 +0000

    Flake8 compliance.

--- act_dr6_cmbonly/tests/test_jax.py
+++ act_dr6_cmbonly/tests/test_jax.py
@@ -3,12 +3,12 @@ This test checks that the differentiable DR6
 likelihood is working as intended.
 """
 import pytest  # noqa F401
-import numpy as np
+import numpy as np  # noqa F401
 
 
 def is_jax_installed():
     try:
-        import jax
+        import jax  # noqa F401
         return True
     except ImportError:
         return False
@@ -18,10 +18,11 @@ def is_jax_installed():
 def test_import():
     from act_dr6_cmbonly import ACTDR6jax  # noqa F401
 
+
 @pytest.mark.skipif(not is_jax_installed, reason="JAX is not installed.")
 def test_jaxlike():
     import act_dr6_cmbonly
-    like = act_dr6_cmbonly.ACTDR6jax()
+    like = act_dr6_cmbonly.ACTDR6jax()  # noqa F401
 
 
 if __name__ == "__main__":
