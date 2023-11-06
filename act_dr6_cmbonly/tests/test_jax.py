d235f6d73e5e927a6a42a26cb1e623436f9fc05a
Hidde Jense <JenseH@cardiff.ac.uk>
2023-11-06 14:50:08 +0000

    Jax optional reqs changes.

--- act_dr6_cmbonly/tests/test_jax.py
+++ act_dr6_cmbonly/tests/test_jax.py
@@ -4,22 +4,20 @@ likelihood is working as intended.
 """
 import pytest  # noqa F401
 import numpy as np  # noqa F401
+import sys
+
+try:
+    import jax  # noqa F401
+except ImportError:
+    pass
 
 
-def is_jax_installed():
-    try:
-        import jax  # noqa F401
-        return True
-    except ImportError:
-        return False
-
-
-@pytest.mark.skipif(not is_jax_installed, reason="JAX is not installed.")
+@pytest.mark.skipif("jax" not in sys.modules, reason="JAX is not installed.")
 def test_import():
     from act_dr6_cmbonly import ACTDR6jax  # noqa F401
 
 
-@pytest.mark.skipif(not is_jax_installed, reason="JAX is not installed.")
+@pytest.mark.skipif("jax" not in sys.modules, reason="JAX is not installed.")
 def test_jaxlike():
     import act_dr6_cmbonly
     like = act_dr6_cmbonly.ACTDR6jax()  # noqa F401
