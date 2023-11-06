5307ccf6dff3fac207488652ed8bfc54bfb07782
Hidde Jense <JenseH@cardiff.ac.uk>
2023-11-06 16:39:19 +0000

    Add differentiable likelihood (#4)
    * Added jax differentiable likelihood.
    
    * Flake8 compliance.
    
    * Added jax tests to workflow.
    
    * Cobaya compliance, fixed import error in jax like import.
    
    * Jax optional reqs changes.
    
    * Added jax tests.
    
    * Minor fixes & code style.
    
    * Path for jax like test.
    
    * Add ell cut for jax test.
    
    * Fix indexing off-by-one in data load.
    
    * Added missing test to default file run.
    
    * Added HMC example.
    
    * Updated readme.
    
    * Typo.
    
    * Typo.

---
+++ act_dr6_cmbonly/tests/test_jax.py
@@ -0,0 +1,71 @@
+"""
+This test checks that the differentiable DR6
+likelihood is working as intended.
+"""
+import pytest  # noqa F401
+import sys
+
+try:
+    import jax  # noqa F401
+    import jax.numpy as np
+    from cosmopower_jax.cosmopower_jax import CosmoPowerJAX as CPJ
+except ImportError:
+    pass
+
+
+@pytest.mark.skipif("jax" not in sys.modules, reason="JAX is not installed.")
+def test_import_jaxlike():
+    from act_dr6_cmbonly import ACTDR6jax  # noqa F401
+
+
+@pytest.mark.skipif("jax" not in sys.modules, reason="JAX is not installed.")
+def test_jaxlike():
+    import act_dr6_cmbonly
+    like = act_dr6_cmbonly.ACTDR6jax()  # noqa F401
+
+
+@pytest.mark.skipif("jax" not in sys.modules, reason="JAX is not installed.")
+def test_jax_load_data():
+    import act_dr6_cmbonly
+    like = act_dr6_cmbonly.ACTDR6jax()
+    like.data_folder = "act_dr6_cmbonly/data"
+    like.load_data()
+
+
+@pytest.mark.skipif("cosmopower_jax" not in sys.modules,
+                    reason="Cosmopower-JAX is not installed.")
+def test_jax_loglike():
+    import act_dr6_cmbonly
+    T_CMB = 2.7255e6
+    cosmo_params = np.array([0.025, 0.12, 0.68, 0.054, 0.97, 3.05])
+
+    emu_tt = CPJ(probe='cmb_tt')
+    emu_te = CPJ(probe='cmb_te')
+    emu_ee = CPJ(probe='cmb_ee')
+    ellfac = emu_tt.modes * (emu_tt.modes + 1.0) / (2.0 * np.pi)
+    cl_tt = (T_CMB) ** 2.0 * emu_tt.predict(cosmo_params) * ellfac
+    cl_te = (T_CMB) ** 2.0 * emu_te.predict(cosmo_params) * ellfac
+    cl_ee = (T_CMB) ** 2.0 * emu_ee.predict(cosmo_params) * ellfac
+
+    cell = np.stack([cl_tt, cl_te, cl_ee], axis=1)
+
+    like = act_dr6_cmbonly.ACTDR6jax()
+    like.data_folder = "act_dr6_cmbonly/data"
+    like.ell_cuts = {
+        "TT": [600, 2508],
+        "TE": [600, 2508],
+        "EE": [500, 2508]
+    }
+    like.tt_lmax = 2508
+    like.load_data()
+
+    logp = like.logp(cell)
+
+    assert np.isclose(logp, -1236.189)
+
+
+if __name__ == "__main__":
+    test_import_jaxlike()
+    test_jax_load_data()
+    test_jaxlike()
+    test_jax_loglike()
